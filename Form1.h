f393db3e5e1e04c56ee270f322a177c439640666
SimonMRod <simon.moreno@estudiantat.upc.edu>
2024-03-28 20:59:06 +0100

    inclosos botons de #11 Donar d'Alta Entitat i #13 ConsultaEntitats

--- Form1.h
+++ Form1.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "ConsultaEntitats.h"
-
+#include "AltaEntitat.h"
 #include "FormBaixaEntitat.h"
 
 namespace CppCLRWinFormsProject {
@@ -38,6 +38,8 @@ namespace CppCLRWinFormsProject {
 			}
 		}
 	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ AltaEntitat;
+	private: System::Windows::Forms::Button^ ConsultaEntitats;
 	protected:
 
 	private:
@@ -54,23 +56,47 @@ namespace CppCLRWinFormsProject {
 		void InitializeComponent(void)
 		{
 			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->AltaEntitat = (gcnew System::Windows::Forms::Button());
+			this->ConsultaEntitats = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(12, 34);
-			this->button1->Name = L"EsborrarEntitat";
+			this->button1->Location = System::Drawing::Point(66, 35);
+			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(154, 21);
 			this->button1->TabIndex = 0;
 			this->button1->Text = L"Esborrar entitat";
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &Form1::EsborrarEntitat_Click);
 			// 
+			// AltaEntitat
+			// 
+			this->AltaEntitat->Location = System::Drawing::Point(66, 86);
+			this->AltaEntitat->Name = L"AltaEntitat";
+			this->AltaEntitat->Size = System::Drawing::Size(154, 23);
+			this->AltaEntitat->TabIndex = 1;
+			this->AltaEntitat->Text = L"AltaEntitat";
+			this->AltaEntitat->UseVisualStyleBackColor = true;
+			this->AltaEntitat->Click += gcnew System::EventHandler(this, &Form1::AltaEntitat_Click);
+			// 
+			// ConsultaEntitats
+			// 
+			this->ConsultaEntitats->Location = System::Drawing::Point(66, 143);
+			this->ConsultaEntitats->Name = L"ConsultaEntitats";
+			this->ConsultaEntitats->Size = System::Drawing::Size(154, 23);
+			this->ConsultaEntitats->TabIndex = 2;
+			this->ConsultaEntitats->Text = L"ConsultaEntitats";
+			this->ConsultaEntitats->UseVisualStyleBackColor = true;
+			this->ConsultaEntitats->Click += gcnew System::EventHandler(this, &Form1::ConsultaEntitats_Click);
+			// 
 			// Form1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(284, 261);
+			this->Controls->Add(this->ConsultaEntitats);
+			this->Controls->Add(this->AltaEntitat);
 			this->Controls->Add(this->button1);
 			this->Name = L"Form1";
 			this->Text = L"Form1";
@@ -83,5 +109,13 @@ namespace CppCLRWinFormsProject {
 		baixa_entitat->Show();
 	}
 
+	private: System::Void AltaEntitat_Click(System::Object^ sender, System::EventArgs^ e) {
+		application::AltaEntitat^ alta_entitat = gcnew application::AltaEntitat();
+		alta_entitat->Show();
+	}
+	private: System::Void ConsultaEntitats_Click(System::Object^ sender, System::EventArgs^ e) {
+		application::ConsultaEntitats^ consulta_entitats = gcnew application::ConsultaEntitats();
+		consulta_entitats->Show();
+	}
 	};
 }
